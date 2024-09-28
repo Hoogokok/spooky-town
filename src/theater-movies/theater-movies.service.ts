fcf7eccbbd5ae72fe236cd90b7f5574935dfb255
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-28 15:56:12 +0900

    feat: TheaterMoviesService에 리포지토리 주입
    - TheaterMoviesService 클래스에 엔티티 리포지토리 주입
    - @InjectRepository 데코레이터를 사용하여 Movie, Theater, MovieTheater 리포지토리 주입
    - 생성자를 통한 의존성 주입 구현

--- src/theater-movies/theater-movies.service.ts
+++ src/theater-movies/theater-movies.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Movie } from './entities/movie.entity';
+import { Theater } from './entities/theater.entity';
+import { MovieTheater } from './entities/movie-theater.entity';
 
 @Injectable()
-export class TheaterMoviesService {}
+export class TheaterMoviesService {
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+    @InjectRepository(Theater)
+    private readonly theaterRepository: Repository<Theater>,
+    @InjectRepository(MovieTheater)
+    private readonly movieTheaterRepository: Repository<MovieTheater>,
+  ) {}
+}
