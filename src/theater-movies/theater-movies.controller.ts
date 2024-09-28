38697980ac9d1a3fb08ceda601fcfd27c3d0fb5e
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-28 19:55:35 +0900

    feat: TheaterMoviesController에 TheaterMoviesService 주입
    - TheaterMoviesController 생성자에 TheaterMoviesService 의존성 주입
    - 영화관 관련 영화 정보 처리를 위한 서비스 연결

--- src/theater-movies/theater-movies.controller.ts
+++ src/theater-movies/theater-movies.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { TheaterMoviesService } from './theater-movies.service';
 
 @Controller('movies/theater')
-export class TheaterMoviesController {}
+export class TheaterMoviesController {
+  constructor(private readonly theaterMoviesService: TheaterMoviesService) {}
+}
