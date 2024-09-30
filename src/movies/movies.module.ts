8d4fc1f1b5c3e61b1e5b7d75f999f6c4d0d76702
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-30 11:03:32 +0900

    feat(movies): MoviesModule에 엔티티 등록
    - Movie, MovieProvider, MovieTheater, Theater 엔티티를 MoviesModule에 등록
    - TypeOrmModule.forFeature에 모든 엔티티 추가

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie.entity';
+import { MovieProvider } from './entities/movie-provider.entity';
+import { MovieTheater } from './entities/movie-theater.entity';
+import { Theater } from './entities/theater.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([Movie, MovieProvider, MovieTheater, Theater])],
   controllers: [],
   providers: []
 })
