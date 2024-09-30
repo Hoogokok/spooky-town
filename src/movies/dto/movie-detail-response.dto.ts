9b9728426f5592e0f4ad2f3528b6d89c4f8f4c8a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-30 13:01:47 +0900

    feat(movies): overview, voteAverage, voteCount, providers, theMovieDbId 필드 MovieDetailResponseDto에 추가

--- src/movies/dto/movie-detail-response.dto.ts
+++ src/movies/dto/movie-detail-response.dto.ts
@@ -1,7 +1,11 @@
 export class MovieDetailResponseDto {
     id: number;
     title: string;
+    overview: string;
     releaseDate: string;
     posterPath: string;
-    providers?: string;
+    voteAverage: number;
+    voteCount: number;
+    providers?: string[];
+    theMovieDbId: number;
   }
