5823f769162db273739a93e21246f381f80d5bfa
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-18 14:57:56 +0900

    test: 테스트용 데이터베이스 설정 파일 추가
    - 인메모리 SQLite 데이터베이스 설정을 위한 test-db.config.ts 파일 생성
    - 테스트에 사용될 엔티티 설정

--- test/test-db.config.ts
+++ test/test-db.config.ts
@@ -1,13 +1,19 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 import { Movie } from '../src/movies/entities/movie.entity';
 import { MovieProvider } from '../src/movies/entities/movie-provider.entity';
 import { NetflixHorrorExpiring } from '../src/movies/entities/netflix-horror-expiring.entity';
+import { MovieTheater } from '../src/movies/entities/movie-theater.entity';
+import { Theater } from '../src/movies/entities/theater.entity';
+import { Review } from '../src/movies/entities/review.entity';
 
-export const testDbConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
+export const testDbConfig: DataSourceOptions = {
+  type: 'better-sqlite3',
   database: ':memory:',
-  entities: [Movie, MovieProvider, NetflixHorrorExpiring],
+  entities: [Movie, MovieProvider, NetflixHorrorExpiring, MovieTheater, Theater, Review],
   synchronize: true,
   logging: false,
+  extra: {
+    foreign_keys: true,
+    dateStrings: true,
+  },
 };
-
