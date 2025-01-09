e5c28708929ae16bbdf6ec2599f666a382489b87
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-10 02:00:02 +0900

    fix: Enhance CORS configuration to support multiple origins
    - Updated the CORS settings in main.ts to allow an array of origins, including FRONTEND_URL, FRONTEND_URL_2, and a fallback for local development.
    - This change improves flexibility for handling requests from different frontend environments.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { AuthGuard } from './auth/auth.guard';
 
 async function bootstrap() {
+  const origins = [
+    process.env.FRONTEND_URL, 
+    process.env.FRONTEND_URL_2, 
+    'http://localhost:5173'
+  ].filter(Boolean);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || process.env.FRONTEND_URL_2 || 'http://localhost:5173',
+    origin: origins,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, X-API-Key, Authorization',
