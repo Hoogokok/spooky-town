aac32cf209a935002f0c9c9872506af5d8448d6c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-09 21:35:04 +0900

    refactor: Replace ecosystem.config.js with ecosystem.config.cjs and update deployment scripts
    - Deleted ecosystem.config.js as it is no longer needed.
    - Introduced ecosystem.config.cjs for application configuration.
    - Updated deployment workflow to reference the new ecosystem.config.cjs file.

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'spooky-town-api',
+        script: 'dist/main.js',
+        env: {
+            NODE_ENV: 'production'
+        }
+    }]
+} 
--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-      name: 'spooky-town',
-      script: 'dist/main.js',
-      env: {
-        NODE_ENV: 'production'
-      }
-    }]
-  } 
